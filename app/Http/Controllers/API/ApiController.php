2b70a7587d95aee678609a193b448db6dce2b22c
kAi YoNg TaN <kAi YoNg TaN>
2017-06-21 22:18:41 +0800

    api controller done

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -102,7 +102,110 @@ class ApiController extends Controller
     		$mimeType = strtolower($mimeParts[0]);
     	}
 
-    	//switch
+    	switch ($mimeType) {
+    		case 'application/json':
+    			$contentType = 'application/json';
+
+    			$content = json_encode($array);
+    			break;
+    		
+    		default:
+    			$contentType = 'application/json';
+    			$content = json_encode([
+    				'error' => [
+    					'code' => static::CODE_INVALID_MIME_TYPE,
+    					'http_code' => 415,
+    					'message' => sprintf('Content of type %s is not supported.', $mimeType),
+    				]
+    			]);
+    			break;
+    	}
+
+    	$response = Response::make($content, $this->statusCode, $headers);
+
+    	$response->header('Content-Type', $contentType);
+
+    	return $response;
     }
 
+    //return error message
+    protected function respondWithError($message, $errorCode)
+    {
+    	if($this->statusCode === 200)
+    	{
+    		trigger_error("error on 200???", E_USER_WARNING);
+    	}
+
+    	return $this->respondWithArray([
+    		'error' => [
+    		'code' => $errorCode,
+    		'http_code' => $this->statusCode,
+    		'message' => $message,
+    		]
+    	]);
+    }
+
+    /**
+	*generate a response with 403 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorForbidden($message = 'Forbidden')
+	{
+		return $this->setStatusCode(403)->respondWithError($message, self::CODE_FORBIDDEN);
+	}
+
+	/**
+	* generate a response with 500 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorInternalError($message = 'Internal Error')
+	{
+		return $this->setStatusCode(500)->respondWithError($message, self::CODE_INTERNAL_ERROR);
+	}
+
+	/**
+	* generate Response with 404 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorNotFound($message = 'Resource Not Found')
+	{
+		return $this->setStatusCode(404)->respondWithError($message, self::CODE_NOT_FOUND);
+	}
+
+	/**
+	* generate Response with 401 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorUnauthorized($message = 'Unauthorized')
+	{
+		return $this->setStatusCode(401)->respondWithError($message, self::CODE_UNAUTHORIZED);
+	}
+
+	/**
+	* generate Response with 400 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorWrongArgs($message = 'Wrong Arguments')
+	{
+		return $this-> setStatusCode(400)->respondWithError($message, self::CODE_WRONG_ARGS);
+	}
+
+	/**
+	* generate Response with 409 HTTP header and a given message
+	* @return Response
+	*/
+	public function errorConflict($message = 'Conflict')
+	{
+		return $this->setStatusCode(409)->respondWithError($message, self::CODE_CONFLICT);
+	}
+
+	/**
+	* generate Response with 204 HTTP header and a given message
+	* @return Response
+	*/
+	public function successNoContent()
+	{
+		$empty = array();
+		return $this->setStatusCode(204)->respondWithArray($empty);
+	}
 }
